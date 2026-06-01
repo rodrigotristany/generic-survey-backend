c631741b84d7829b1ed46d7f2d599ee1905eaa1d
Rodrigo Tristany <rodrigotristany@gmail.com>
2026-06-01 19:38:57 -0300

    RT: pydantic config updated

--- app/config.py
+++ app/config.py
@@ -1,16 +1,15 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+
     ENV: str = "local"
     DATABASE_URL: str
     SECRET_KEY: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
     OTP_EXPIRE_MINUTES: int = 10
-
-    class Config:
-        env_file = ".env"
 
 
 settings = Settings()
